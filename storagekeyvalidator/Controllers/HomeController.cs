fe4c6c379c998eec36e989ac11ca7036fed3f5d3
Mohammed Fasiulla <mofasiul@microsoft.com>
2020-01-17 11:06:46 -0800

    siteextension-version2

--- storagekeyvalidator/Controllers/HomeController.cs
+++ storagekeyvalidator/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Configuration;
 using System.Web.Mvc;
@@ -19,7 +20,7 @@ namespace storagekeyvalidator.Controllers
             string AzureWebJobsStorage = WebConfigurationManager.AppSettings["AzureWebJobsStorage"];
             string WEBSITE_CONTENTAZUREFILECONNECTIONSTRING = WebConfigurationManager.AppSettings["WEBSITE_CONTENTAZUREFILECONNECTIONSTRING"];
 
-
+          
             Dictionary<string, string> dictConnections = new Dictionary<string, string>();
             dictConnections.Add("AzureWebJobsDashboard", AzureWebJobsDashboard);
             dictConnections.Add("AzureWebJobsStorage", AzureWebJobsStorage);
@@ -55,40 +56,53 @@ namespace storagekeyvalidator.Controllers
                 {
                     if (CloudStorageAccount.TryParse(item.Value, out storageAccount))
                     {
-                        try
+
+                        if (blobonly(storageAccount))
                         {
-                            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-                            CloudBlobContainer cloudBlobContainer =   blobClient.GetContainerReference("quickstartblobs" + Guid.NewGuid().ToString());
-                            var results =  cloudBlobContainer.ListBlobsSegmented(null, null);                          
-
-
                             DataRow row = tblKey.NewRow();
                             row["StorageKey"] = item.Key;
-                            row["Result"] = "succeeded";
-                            row["Description"] = "The storage connection string is valid and no further action needed";
+                            row["Result"] = "Failed";
+                            row["Description"] = "Function app requires a general Azure Storage account, which supports Azure Blob, Queue, Files, and Table storage.Refer <a href='https://docs.microsoft.com/en-us/azure/azure-functions/functions-scale#storage-account-requirements' target='_blank'>Storage Requirements</a>" ;
                             tblKey.Rows.Add(row);
 
-                          
                         }
-                        catch (Exception ex)
+                        else
                         {
-                            if (ex.Message.Contains("403"))
+                            try
                             {
-                                DataRow row = tblKey.NewRow();
-                                row["StorageKey"] = item.Key;
-                                row["Result"] = "Failed";
-                                row["Description"] = "The storage connection string account Key is invalid, the server failed to authenticate the request.Please contact your admin to generate a new key";
-                                tblKey.Rows.Add(row);
-                            }
-                            
-                            if(ex.Message.Contains("404"))
-                            {
+                                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                                CloudBlobContainer cloudBlobContainer = blobClient.GetContainerReference("testblob" + Guid.NewGuid().ToString());
+                                var results = cloudBlobContainer.ListBlobsSegmented(null, null);
+
 
                                 DataRow row = tblKey.NewRow();
                                 row["StorageKey"] = item.Key;
-                                row["Result"] = "Succeeded";
+                                row["Result"] = "succeeded";
                                 row["Description"] = "The storage connection string is valid and no further action needed";
                                 tblKey.Rows.Add(row);
+
+
+                            }
+                            catch (Exception ex)
+                            {
+                                if (ex.Message.Contains("403"))
+                                {
+                                    DataRow row = tblKey.NewRow();
+                                    row["StorageKey"] = item.Key;
+                                    row["Result"] = "Failed";
+                                    row["Description"] = "The storage connection string account Key is invalid, the server failed to authenticate the request. The access keys have either expired or have been revoked by your admin. Please contact your admin to generate a new storage connection key. Refer <a href= 'https://docs.microsoft.com/en-us/azure/azure-functions/functions-app-settings#azurewebjobsstorage' target='_blank'> Function Keys app settings </a> and <a href='https://docs.microsoft.com/en-us/azure/azure-functions/functions-scale#storage-account-requirements' target='_blank'>Storage Requirements</a>";
+                                    tblKey.Rows.Add(row);
+                                }
+
+                                if (ex.Message.Contains("404"))
+                                {
+
+                                    DataRow row = tblKey.NewRow();
+                                    row["StorageKey"] = item.Key;
+                                    row["Result"] = "Succeeded";
+                                    row["Description"] = "The storage connection string is valid and no further action needed";
+                                    tblKey.Rows.Add(row);
+                                }
                             }
                         }
                     }
@@ -97,7 +111,7 @@ namespace storagekeyvalidator.Controllers
                         DataRow row = tblKey.NewRow();
                         row["StorageKey"] = item.Key;
                         row["Result"] = "Failed";
-                        row["Description"] = "The storage connection string couldnt be parsed, please check the syntax of the string" ;
+                        row["Description"] = "The storage connection string couldnt be parsed, check the syntax of the string. you can try generating a new connection string for the storage account and reconfigure the app setting. Please refer <a href= 'https://docs.microsoft.com/en-us/azure/azure-functions/functions-app-settings#azurewebjobsstorage' target='_blank'> Function Keys app settings </a> and <a href='https://docs.microsoft.com/en-us/azure/azure-functions/functions-scale#storage-account-requirements' target='_blank'>Storage Requirements</a>";
                         tblKey.Rows.Add(row);
                     }
                 }
@@ -111,7 +125,7 @@ namespace storagekeyvalidator.Controllers
                 DataRow row = tblKey.NewRow();
                 row["StorageKey"] = "NULL";
                 row["Result"] = "No Storage Keys Found";
-                row["Description"] = "Azure Function runtime needs storage keys to function properly, please refer https://docs.microsoft.com/en-us/azure/azure-functions/functions-app-settings#azurewebjobsstorage";
+                row["Description"] = "Azure Function runtime must be associated with a storage account, please refer <a href= 'https://docs.microsoft.com/en-us/azure/azure-functions/functions-app-settings#azurewebjobsstorage' target='_blank'> Function Keys app settings </a> and <a href='https://docs.microsoft.com/en-us/azure/azure-functions/functions-scale#storage-account-requirements' target='_blank'>Storage Requirements</a>";
                 tblKey.Rows.Add(row);
 
             }
@@ -119,6 +133,24 @@ namespace storagekeyvalidator.Controllers
 
 
             return View(tblKey);
+        }
+
+        public bool blobonly(CloudStorageAccount storage)
+        {
+            bool result = false;
+            try
+            {
+                Dns.GetHostEntry(storage.BlobEndpoint.Host);
+                Dns.GetHostEntry(storage.FileEndpoint.Host);
+                Dns.GetHostEntry(storage.QueueEndpoint.Host);
+                Dns.GetHostEntry(storage.TableEndpoint.Host);
+            }
+            catch
+            {
+                result = true;
+            }
+
+            return result;
         }
 
     }
